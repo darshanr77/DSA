a242e7afe27af21d6708d2783059807fc0831138
Darshan R <darshankok46@gmail.com>
2026-01-14 22:32:18 +0530

    Clarify comment on pass by reference with pointerscomment
    Updated comment to clarify the functionality of pass by reference using pointers.

--- POINTERS/passbyref.cpp
+++ POINTERS/passbyref.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-// pass by  reference using pointers  (replaces the original main value of a or b with the void function's value)
+// pass by  reference using pointers  (replaces the original main value of a or b with the void function's value or in simple main a-5 and in void change its like a-10 then it will replace 10 in main )
 
 void changeA(int *ptr){
     *ptr = 10;
