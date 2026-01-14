d6d62c9a8d1b2112c2753a40484051f97f73d54c
darshankok7 <darshankok7@gmail.com>
2026-01-14 21:46:34 +0530

    swap pointers

--- POINTERS/code.cpp
+++ POINTERS/code.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int var = 557;      // A simple integer (using your VTU subject code!)
     int* ptr = &var;    // A pointer storing the address of var
+    int** ptrr = &ptr;  // A double pointer storing the address of ptr
 
 
 
@@ -19,6 +20,27 @@ int main() {
     // cout << "Address stored in ptr: " << ptr << endl;
     // cout << "Value retrieved via ptr (*ptr): " << *ptr << endl;
 
+        // Data Type	Size of Variable (Typical)	Size of Pointer to Type (type*)
+        // char	1 byte	8 bytes (on 64-bit)
+        // int	4 bytes	8 bytes (on 64-bit)
+        // float	4 bytes	8 bytes (on 64-bit)
+        // double	8 bytes	8 bytes (on 64-bit)
+        // long long	8 bytes	8 bytes (on 64-bit)
+        // void	N/A (cannot declare)	8 bytes (on 64-bit)
+
+
+        // int** a is a double pointer which stores the address of an int* pointer variable.
+
+        cout << "size fo char : " << sizeof(char) << " bytes" << endl;
+        cout << "size fo int : " << sizeof(int) << " bytes" << endl;
+        cout << "size fo float : " << sizeof(float) << " bytes" << endl;
+        cout << "size fo double : " << sizeof(double) << " bytes" << endl;
+        cout << "size fo long long : " << sizeof(long long) << " bytes" << endl;
+        cout << "size fo void* : " << sizeof(void*) << " bytes" << endl;    
+
+
+        cout << "double pointer :"<< **ptrr << endl;
+        cout << "single pointer :"<< *ptr << endl;
 
     return 0;
 }
