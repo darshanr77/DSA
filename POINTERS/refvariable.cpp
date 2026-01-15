1dc81d88fa39be8fb5416f341db37b8f2c3eb39f
Darshan R <darshankok46@gmail.com>
2026-01-15 08:56:15 +0530

    ref variable

--- POINTERS/refvariable.cpp
+++ POINTERS/refvariable.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 
  int main () {
-   
+    int a = 10;
+    int &b = a;
+
+    cout << a << " = " << b << endl;    // both the adres of a and b are same , b is just a ref variable of a
+                                        // ref variable should be first verified eg-> int &h = k;
 
     return 0;
  }
