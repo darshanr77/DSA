b2b28d62c64943804e1aa747e4d3fb0302e8c4f9
darshanr77 <darshankok46@gmail.com>
2026-01-20 22:02:53 +0530

    counting sort for descending order

--- BASIC SORTING ALGO/prob2.cpp
+++ BASIC SORTING ALGO/prob2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <algorithm>
+#include <climits>
 using namespace std;
 
 void printA(int* arr, int n) {
@@ -28,24 +28,62 @@ void bubbleSort(int*arr , int n){
 //insertion sort for descending order
 
 void insertionSort(int*arr,int n){
-    for(int i=0; i<n; i++){
+    for(int i=1; i<n; i++){
         int curr = arr[i];
         int prev = i-1;
 
         while(prev >= 0 && arr[prev] < curr){
-            swap(arr[prev],arr[prev+1]);
+            arr[prev+1] = arr[prev];
             prev--;                             // 
         }
-        arr[prev] = curr;
+        arr[prev+1] = curr;
     }
     printA(arr,n);
 }
+
+
+//couting sort for descending order
+
+void countSort(int* arr, int n) {
+
+    int maxValue = INT_MIN;
+    int minValue = INT_MAX;
+
+    // 🔹 Find min and max value in array
+    for(int i = 0; i < n; i++){
+        minValue = min(minValue, arr[i]);
+        maxValue = max(maxValue, arr[i]);
+    }
+
+    int range = maxValue - minValue + 1;
+
+    int freq[100000] = {0};   // range & initialized to 0
+
+    // 🔹 Store frequency of each element in freq[] array
+    for(int i = 0; i < n; i++){
+        freq[arr[i] - minValue]++;   // storing the frequency of ith element in frequency array
+    }
+
+    int j = 0;  // index for original array arr[]
+
+    // 🔹 Rebuild sorted array using frequency array
+    for(int i = range - 1; i >= 0; i--){
+        while(freq[i] > 0){     // if element has frequency then add it in original array and decrease its frequency in freq array
+            arr[j++] = i + minValue;  // OR arr[j] = i; j = j+1;
+            freq[i]--;   // after adding the element from freq to arr then we have to decrease the freq of the element
+        }
+    }
+
+    printA(arr, n);
+}
+
+
 
 int main() {
 
     int arr[10] = {3,6,2,1,8,7,4,5,3,1};
     int n = sizeof(arr)/sizeof(int);
 
-    insertionSort(arr,n);
+    countSort(arr,n);   // you can also call bubbleSort or insertionSort
     return 0;
 }
