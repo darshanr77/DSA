c0128797712a265753fa4bf0946ba99704dc04e2
darshanr77 <darshankok46@gmail.com>
2026-01-20 21:26:11 +0530

    insertion sort for descending order

--- BASIC SORTING ALGO/prob2.cpp
+++ BASIC SORTING ALGO/prob2.cpp
@@ -24,11 +24,28 @@ void bubbleSort(int*arr , int n){
     printA(arr,n);
 }
 
+
+//insertion sort for descending order
+
+void insertionSort(int*arr,int n){
+    for(int i=0; i<n; i++){
+        int curr = arr[i];
+        int prev = i-1;
+
+        while(prev >= 0 && arr[prev] < curr){
+            swap(arr[prev],arr[prev+1]);
+            prev--;                             // 
+        }
+        arr[prev] = curr;
+    }
+    printA(arr,n);
+}
+
 int main() {
 
     int arr[10] = {3,6,2,1,8,7,4,5,3,1};
     int n = sizeof(arr)/sizeof(int);
 
-    bubbleSort(arr,n);
+    insertionSort(arr,n);
     return 0;
 }
