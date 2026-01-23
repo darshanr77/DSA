8c6e62677d5123942ed98c21852a2d6f5f752a12
darshanr77 <darshankok46@gmail.com>
2026-01-23 07:17:19 +0530

    transpose

--- 2DARRAY/3.cpp
+++ 2DARRAY/3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//transpose of a matrix
+
 int main() {
     int r, c;
     int mat[10][10], trans[10][10];
@@ -18,14 +20,14 @@ int main() {
     // Finding transpose
     for(int i = 0; i < r; i++) {
         for(int j = 0; j < c; j++) {
-            trans[j][i] = mat[i][j];
+            trans[j][i] = mat[i][j];   // assigning mat[i][j] to trans and its not transposed yet
         }
     }
 
     cout << "Transpose of the matrix:\n";
     for(int i = 0; i < c; i++) {
         for(int j = 0; j < r; j++) {
-            cout << trans[i][j] << " ";
+            cout << trans[i][j] << " ";  // here it  is transposed
         }
         cout << endl;
     }
