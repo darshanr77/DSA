ea46948cc422320c1fe1b827cea05042b351048f
darshanr77 <darshankok46@gmail.com>
2026-01-24 10:44:35 +0530

    tsring member functions

--- CHARRSTR/strmemfunctions.cpp
+++ CHARRSTR/strmemfunctions.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main () {
 
-    string str = "apna school";
+    string str = "apna school and college and";
 
 
     //both are same
@@ -14,8 +14,11 @@ int main () {
     cout << str.at(3)<<endl;
 
     //sub string
-    cout << str.substr(0,5);
+    cout << str.substr(0,5)<<endl;
    
+    cout << str.find("and")<<endl; // output will be the starting index of and
+
+    cout << str.find("and",23)<<endl; // if there are two or more same word then we can verify it from that index 
 
 
     return 0;
