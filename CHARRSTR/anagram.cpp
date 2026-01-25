de1806e935f79a10a5945a72f07c4ad4de280457
darshanr77 <darshankok46@gmail.com>
2026-01-25 08:12:29 +0530

    anagarm for two strings using couont arrayy

---
+++ CHARRSTR/anagram.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+bool anagram(string str1,string str2){
+
+    if(str1.length() != str2.length()){ // both strings must be same if it has to be anagaram
+        cout << "Not an anagram \n";
+        return false;
+    }
+
+    int count[50]={0};
+
+    for(int i=0; i<str1.length(); i++){
+        int idx =  str1[i]-'a';         // adding indexs in count
+        count[idx]++;
+    }
+    for(int i=0; i<str2.length(); i++){
+        int idx = str2[i]-'a';
+         if(count[idx] == 0){           // check whether the idx is present if not not a valid anagarm
+            return false;
+         }
+         count[idx]--;                  // if idx found in count then --
+    }
+
+    cout << "Valid anagarms \n";
+    return true;
+}
+
+int main () {
+
+    string str1 = "anagram";
+    string str2 = "naaram";
+
+    anagram(str1,str2);
+  
+
+    return 0;
+}
