623d3f60c8acfcf0753bc18f0b3f2998a47a54f7
darshanr77 <darshankok46@gmail.com>
2026-01-29 06:57:33 +0530

    traip rain water

--- ARRAY/traprain.cpp
+++ ARRAY/traprain.cpp
@@ -6,25 +6,28 @@ using namespace std;
 
 int rainWater(int height[] , int n){
     int leftMax[100000]; int rightMax[100000];
-    rightMax[n-1] = height[n-1];              // last element
-    leftMax[0] = height[0];                   // 1st element
+      if(n ==0) return 0;    
+
+    rightMax[n-1] = height[n-1];               // initializing leftmax[0] to arrheight[n-1] or last element
+    leftMax[0] = height[0];  
+               
 
     for (int i=1 ; i< n ; i++){                     // left max starts with 1 bcoz 0 is initialized as leftMax
-        leftMax[i] = max(leftMax[i-1],height[i]);   //  i-1  bcoz if there i-1 element is big than 
+        leftMax[i] = max(leftMax[i-1],height[i]);   //  comparing left and 2nd left element
     }
 
     for(int i=n-2 ; i>=0 ; i--){                     // right max starts with n-2 bcoz n-1 is initialized as rightMax
-        rightMax[i] = max(rightMax[i+1],height[i]);  //  i+1  bcoz if there i+1 element is big than i  
+        rightMax[i] = max(rightMax[i+1],height[i]);  //  comparing 2nd right and last element
     }
 
 
-    int watertrapped = 0;
+    int watertrapped = 0; 
     int currWater;
     for(int i=0 ; i<n ; i++){
-       currWater = min(leftMax[i],rightMax[i] - height[i]);
+       currWater = min(leftMax[i],rightMax[i]) - height[i];
 
        if(currWater > 0){
-            watertrapped += currWater;                 // eg -> 0 = 0 + 3;    ,   3 = 3 + 2;
+            watertrapped += currWater;                 // eg -> 0 = 0 + 3;    ,   3 = 3 + 2;  and we are calcualting total water trapped in every index not max
        }
        
     }
