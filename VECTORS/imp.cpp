b2b1f099fc524d03a262ece126fcbaee6b028c47
Darshan R <darshankok46@gmail.com>
2026-02-03 21:18:41 +0530

    Update imp.cpp

--- VECTORS/imp.cpp
+++ VECTORS/imp.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-// new keyword is used to allocate new memory
+// new keyword is used to allocate new memoryy
 
 //which ever memory we create using new it will be created in heap memory so we must delete that otherwise it leads to memory leak
 // that means in heap the memory will be created largely and if we not delete it then heap memory will be left with some memory while using that our system may crash
