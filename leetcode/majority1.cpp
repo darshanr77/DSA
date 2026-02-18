28946e87b5b9d5666529fe3bd365760112af93de
Darshan <darshankok46@gmail.com>
2026-02-18 06:02:19 +0530

    calculation of time complexcity

--- leetcode/majority1.cpp
+++ leetcode/majority1.cpp
@@ -12,3 +12,17 @@ public:
         }
     }
 };
+
+int majorityElement(vector<int>& nums) {
+    int candidate = 0;
+    int count = 0;
+
+    for (int i : nums) {
+        if (count == 0) {
+            candidate = i;
+        }
+        count += (i == candidate) ? 1 : -1;
+    }
+
+    return candidate;
+}
