d5a33a693ed6cc50b7a649c0a72c056f005f9dfd
Darshan <darshankok46@gmail.com>
2026-03-04 06:39:20 +0530

    standard template library

--- LL/removecycle.cpp
+++ LL/removecycle.cpp
@@ -50,37 +50,76 @@ public:
     void removeCycle(){
         Node* slow = head;
         Node* fast = head;
+        bool isCyclic = false;
+        cout << "cycle remove is called\n";
 
+
+        // to meet slow and fast at one point
         while( fast != NULL && fast->next != NULL){
             slow = slow->next;      // +1
             fast = fast->next->next;// +2
+
+            // to detect cycle
+            if( slow == fast ){
+                cout << "cycle exist in this ll\n";
+                isCyclic = true;
+                break;
+        }
+        }
+      
+        if(!isCyclic){
+            cout << "cycle does not exist\n";
+            return;
         }
 
-        if( slow = fast ){
-            cout << "cycle exist n this ll\n";
-        }
-        else{
-            cout << "cycle does not exist\n";
+        slow = head;
+
+        // full cyclic like circle
+        if (slow == fast) {   // special case : tail -> head
+
+            while (fast->next != slow) { // this makes the fast connect to first ele of ll then remove it
+                fast = fast->next;
+            }
+
+        fast->next = NULL;   // remove cycle
+        }       
+
+        else { // in this the fast->next is connect with other center ele not head or tail
+
+            Node* prev = fast;
+
+            while (slow != fast) {
+                slow = slow->next; // +1
+                prev = fast;       // here prev stays back of fast the fast moves to next node
+                fast = fast->next; // +1
+            }  // slow and fast will meet at one point and we have to del prev->next so that cycle get deleted
+
+            prev->next = NULL;   // remove cycle
         }
 
     }
 };
 
-int main( ){
+int main(){
 
     List ll;
 
     ll.push_front(5);
-     ll.printList();
     ll.push_front(4);
-     ll.printList();
     ll.push_front(3);
-     ll.printList();
     ll.push_front(2);
-     ll.printList();
     ll.push_front(1);
 
-    ll.printList();
+    ll.printList();  // normal print
+
+    // Create cycle
+    ll.tail->next = ll.head;
+
+    // DO NOT PRINT HERE (infinite loop)
+
+    ll.removeCycle();
+
+    ll.printList();  // now safe
 
     return 0;
 }
---
+++ LL/stl.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+using namespace std;
+
+void printList(list<int> ll){
+     list<int>::iterator itr;
+    for(itr = ll.begin(); itr != ll.end(); itr++){
+        cout << (*itr) << "-> ";
+    }
+    cout << "NULL\n";
+}
+
+
+int main () {
+
+    list<int> ll;
+
+    ll.push_front(4);
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+
+    printList(ll);
+
+    cout << "size of ll :" << ll.size()<<endl;
+    cout << "head :" << ll.front()<<endl;
+    cout << "tail :" << ll.back()<<endl;
+
+    return 0;
+}
