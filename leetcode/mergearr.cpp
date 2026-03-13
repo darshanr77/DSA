fa2240c5041824cec5463fc0c087bc52feaa36fe
Darshan R <darshankok7@gmail.com>
2026-03-13 14:06:29 +0530

    merging two arr and storing it in 1st arr

--- leetcode/mergearr.cpp
+++ leetcode/mergearr.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
-  // using three pointers
 
 void mergeNum(int* num1,int* num2,int n,int m){
 
-    int i = m-1;
-    int j = n-1;
+    int i = n-1;
+    int j = m-1;
     int k = m+n-1;
 
     while(i>=0 && j>=0){
 
-        //for checking large value and place it in last of num1
         if(num1[i] > num2[j]){
-            num1[k] = num1[i];
+            num1[k] = num1[i]; // placing large element in num1[k] ie last
             i--;
         }
-        else{ // num1[i] < num2[j]
-            num1[k] =  num2[j];
+        else{
+            num1[k] = num2[j];
             j--;
         }
         k--;
     }
 
-    //for printing all ele of num2 in num1[k]
     while(j>=0){
         num1[k] = num2[j];
         j--;
@@ -30,21 +27,19 @@ void mergeNum(int* num1,int* num2,int n,int m){
     }
 }
 
-
 int main() {
 
-    int num1[] = {1,2,3,4,0,0};
-    int m = 4;
+    int num1[6] = {1,2,3};   // extra space
+    int n = 3;
 
-    int num2[] = {1,2};
-    int n = 2;
+    int num2[] = {4,5,6};
+    int m = 3;
 
     mergeNum(num1,num2,n,m);
 
-    for(int i=0; i<m+n; i++){ // size of merged num1 ie m+n is important
-        cout << num1[i]<< ","<<endl;
+    for(int i=0; i<m+n; i++){
+        cout << num1[i] << ",";
     }
-
 
     return 0;
 }
